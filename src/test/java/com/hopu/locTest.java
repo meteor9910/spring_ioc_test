e7908c2558530d94dd509834a681562de7990b1a
zhangyang <a16602705424@outlook.com>
2020-11-03 14:47:06 +0800

    改进

--- src/test/java/com/hopu/locTest.java
+++ src/test/java/com/hopu/locTest.java
@@ -1,5 +1,19 @@
 package com.hopu;
 
-public class locTest {
+import com.hopu.ioc.User;
+import com.sun.org.apache.bcel.internal.util.ClassPath;
+import javafx.application.Application;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+public class locTest {
+    @Test
+    public void testIoc() {
+        ApplicationContext act =
+                new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        User user = (User) act.getBean("user");
+        user.study();
+
+    }
 }
